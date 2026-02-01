97f668d454709bb10b8638b2fd59ff03fb00c67e
Seamus Ryan <seamusryan787@gmail.com>
2026-01-31 19:31:50 -0500

    project

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,15 +1,24 @@
-const clothingItems = require('../models/clothingItemsModel');
+const clothingItems = [
+    { id: 1, name: 'T-Shirt', category: 'tops', price: 19.99 },
+    { id: 2, name: 'Jeans', category: 'bottoms', price: 49.99 },
+    { id: 3, name: 'Jacket', category: 'outerwear', price: 79.99 }
+];
 
-// Example function to get all clothing items
-const getAllClothingItems = (req, res) => {
-    clothingItems.find({}, (err, items) => {
-        if (err) {
-            return res.status(500).json({ message: 'Error retrieving items' });
-        }
-        res.status(200).json(items);
-    });
+exports.getAllClothingItems = (req, res) => {
+    res.json(clothingItems);
 };
 
-module.exports = {
-    getAllClothingItems,
+exports.getClothingItemById = (req, res) => {
+    const item = clothingItems.find(item => item.id === parseInt(req.params.id));
+    if (!item) return res.status(404).json({ message: 'Item not found' });
+    res.json(item);
+};
+
+exports.createClothingItem = (req, res) => {
+    const newItem = {
+        id: clothingItems.length + 1,
+        ...req.body
+    };
+    clothingItems.push(newItem);
+    res.status(201).json(newItem);
 };
