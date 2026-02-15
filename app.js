a396db1a0c98f5588ae055d0e3d6192639bf2455
Seamus Ryan <seamusryan787@gmail.com>
2026-02-15 00:47:46 -0500

    sprint 13

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 const express = require("express");
-const auth = require("./middlewares/auth");
-const { login, createUser } = require("./controllers/users");
-const userRoutes = require("./routes/users");
-const itemRoutes = require("./routes/items");
+const routes = require("./routes"); // main router
 
 const app = express();
 
 app.use(express.json());
 
-// Public routes
-app.post("/signin", login);
-app.post("/signup", createUser);
-app.get("/items", itemRoutes); // GET /items is public
-
-// Apply auth to everything below
-app.use(auth);
-
-// Protected routes
-app.use("/users", userRoutes);
-app.use("/items", itemRoutes); // all other item routes are protected
+// Mount all routes
+app.use("/", routes);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 const router = require("express").Router();
+const auth = require("../middlewares/auth");
 
 const userRouter = require("./users");
 const itemsRouter = require("./clothingItems");
+const { login, createUser } = require("../controllers/users");
 
-// Mount routers
+// Public routes
+router.post("/signin", login);
+router.post("/signup", createUser);
+router.get("/items", itemsRouter); // GET /items is public
+
+// Protect everything below this line
+router.use(auth);
+
+// Protected routes
 router.use("/users", userRouter);
-router.use("/items", itemsRouter);
+router.use("/items", itemsRouter); // all other item routes are protected
 
 module.exports = router;
