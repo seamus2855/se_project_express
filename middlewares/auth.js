675ee02ea6ce5fbb04b0bdf59ea0e51ac06e4969
Seamus Ryan <seamusryan787@gmail.com>
2026-02-15 00:37:43 -0500

    sprint 13

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,19 @@
 const jwt = require("jsonwebtoken");
 const { UNAUTHORIZED } = require("../../utils/errors");
 const { JWT_SECRET } = require("../../utils/config");
-
+ 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
+ 
   // Expect header: Authorization: Bearer <token>
   if (!authorization || !authorization.startsWith("Bearer ")) {
     return res
       .status(UNAUTHORIZED)
       .json({ message: "Authorization required" });
   }
-
+ 
   const token = authorization.replace("Bearer ", "");
-
+ 
   try {
     const payload = jwt.verify(token, JWT_SECRET);
     req.user = payload; // now req.user._id is available
@@ -24,3 +24,4 @@ module.exports = (req, res, next) => {
       .json({ message: "Invalid or expired token" });
   }
 };
+ 
