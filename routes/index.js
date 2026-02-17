76821674a2561fcf9620d2c6f0dc15e921f8c38a
Seamus Ryan <seamusryan787@gmail.com>
2026-02-16 22:21:43 -0500

    sprint 13

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,13 @@ const { getAll } = require("../controllers/clothingItems");
 // Public routes
 router.post("/signin", login);
 router.post("/signup", createUser);
-router.get("/items", getAll); // ✔ ONLY getAll is public
+router.get("/items", getAll); // public GET /items
 
 // Protect everything below this line
 router.use(auth);
 
 // Protected routes
 router.use("/users", userRouter);
-router.use("/items", itemsRouter); // ✔ all other item routes protected
+router.use("/items", itemsRouter);
 
 module.exports = router;
